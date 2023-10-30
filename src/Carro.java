9b7dedcecd44911d9edd03c648c7581e5ea2dd32
Fernando Manuel Pinho <109285124+fernandopinho17@users.noreply.github.com>
2023-10-30 20:53:07 +0100

    Update Carro.java
    adicionar elementos

--- src/Carro.java
+++ src/Carro.java
@@ -1,2 +1,42 @@
-package PACKAGE_NAME;public class Carro {
+public class Carro {
+
+    private  int  codigo;
+
+    private  String nome;
+
+    private  String endereco;
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    //metudos
+
+//    ndnvdvnfxovnmxfnxovn;
+//    vinvovnainqsnsiznc;
+//    sf,+eamfa+gma+ga´g,va´;
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public void cadastrarEndereco(String endereco) {
+        setEndereco(endereco);
+    }
 }
